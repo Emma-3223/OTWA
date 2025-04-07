232297743e5cd5bf184fa977a253da8f05f1fd33
Emma-90844 <enabojohn@gmail.com>
2025-04-07 12:45:41 +0300

    Created DB COnnection to Mongo

---
+++ api/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+
+
+// Connecting the server to the backend
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+
+
+
+
+
+// App port
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
+
+
+
+
+
+
+
+
+
