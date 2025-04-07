a31ab38ef2704f44242edac1119b0750d0655456
Emma-90844 <enabojohn@gmail.com>
2025-04-07 14:39:37 +0300

    Created Signup API route

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 
 
 
 
 const app = express();
+app.use(express.json())
 dotenv.config();
 
 
@@ -29,6 +31,7 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRouter);
+app.use('api/auth', authRouter)
 
 
 
